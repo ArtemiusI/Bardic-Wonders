fc45534c226bb760e659af3da3d7de88e19ef0f9
ArtemiusI <whuangx10@gmail.com>
2025-09-09 09:38:28 +0800

    added experimental mage/bard multi-class component

---
+++ BardicWonders/lua/m_c0mgbd.lua
@@ -0,0 +1,21 @@
+function ArtisanMageBardActionbarListener(config, state)
+   if 
+      state == 13
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x4000
+      then
+      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.BARD_SONG)
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.QUICK_SPELL_1)
+   end
+end
+EEex_Actionbar_AddListener(ArtisanMageBardActionbarListener)
+
+function ArtisanFighterMageBardActionbarListener(config, state)
+   if 
+      state == 10
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x4001
+      then
+      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.BARD_SONG)
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.QUICK_SPELL_1)
+   end
+end
+EEex_Actionbar_AddListener(ArtisanFighterMageBardActionbarListener)
