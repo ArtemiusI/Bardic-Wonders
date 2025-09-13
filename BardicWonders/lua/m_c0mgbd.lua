ae4ac49cda3f3a9207e31ccaf52fcea85742795a
ArtemiusI <whuangx10@gmail.com>
2025-09-13 16:18:20 +0800

    added illusionist/bard variant for gnomes

--- BardicWonders/lua/m_c0mgbd.lua
+++ BardicWonders/lua/m_c0mgbd.lua
@@ -19,3 +19,25 @@ function ArtisanFighterMageBardActionbarListener(config, state)
    end
 end
 EEex_Actionbar_AddListener(ArtisanFighterMageBardActionbarListener)
+
+function ArtisanIllusionistBardActionbarListener(config, state)
+   if 
+      state == 13
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x4002
+      then
+      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.BARD_SONG)
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.QUICK_SPELL_1)
+   end
+end
+EEex_Actionbar_AddListener(ArtisanIllusionistBardActionbarListener)
+
+function ArtisanFighterIllusionistBardActionbarListener(config, state)
+   if 
+      state == 10
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x4003
+      then
+      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.BARD_SONG)
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.QUICK_SPELL_1)
+   end
+end
+EEex_Actionbar_AddListener(ArtisanFighterIllusionistBardActionbarListener)
